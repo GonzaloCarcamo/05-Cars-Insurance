d8b9a90498436b2a842a953bfc4dbd35d6ee99d1
goncmodev <gon.cmo.dev@gmail.com>
2021-06-13 21:13:55 +0100

    Validate form

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ UI.prototype.llenarOpciones = () => {
         for(let i = max; i > min; i--){
                 let option = document.createElement('option');
                 option.value = i;
-                option.textContent = 1;
+                option.textContent = i;
                 selectYear.appendChild(option);
         }
 }
@@ -29,3 +29,28 @@ console.log(ui);
 document.addEventListener('DOMContentLoaded', () => {
         ui.llenarOpciones(); // LLena el select con los años ...
 } )
+
+eventListeners();
+function eventListeners() {
+        const formulario = document.querySelector('#cotizar-seguro');
+        formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+        e.preventDefault();
+
+        // Leer la marca seleccionada
+        const marca = document.querySelector('#marca').value;
+
+        // Leer el año seleccionado
+        const year = document.querySelector('#year').value;
+
+        // Leer el tipo de cobertura 
+        const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+        if(marca === '' || year === '' || tipo === ''){
+                console.log('No pasó validación');
+        }else{
+                console.log('Si pasó validación');
+        }
+}
